42d9c878e66c1daeaa071d4db531b631a609a139
Abhishekbharambe <123597077+AbhishekBharambe2002@users.noreply.github.com>
2024-02-23 18:13:57 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const connectDB = async () =>{
-    try{
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
-         console.log(`mongo db connected:${conn.connection.host}`)
-    }catch(error){
-       console.log(`Error:${error.message}`);
-       process.exit();
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        });
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1); // Exit process with failure
     }
 };
 
