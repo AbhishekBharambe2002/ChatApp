8da9398bc8f2ab0f949c4f51232a6a3e6f440288
Abhishekbharambe <123597077+AbhishekBharambe2002@users.noreply.github.com>
2024-02-23 19:41:10 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () =>{
     try{
-        const conn = await mongoose.connect('mongodb://127.0.0.1:27017/chatlogin');
+        const conn = await mongoose.connect('mongodb+srv://ChatApp:Strikemachine3@chatapp.iqsgyfs.mongodb.net/');
          console.log(`mongo db connected:${conn.connection.host}`)
     }catch(error){
        console.log(`Error:${error.message}`);
